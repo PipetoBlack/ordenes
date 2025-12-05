684c556687e27f9322f4b018a44bcf5a54298f6d
PipetoBlack <fel.navarroc@hotmail.com>
2025-12-05 11:30:18 -0300

    publico

--- src/main/java/com/petsocity/orders/controller/OrderController.java
+++ src/main/java/com/petsocity/orders/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
